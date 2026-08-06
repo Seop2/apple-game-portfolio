e25bba672ac25390693fcb06776f4e179b0f0dd1
Seop2 <lk9050@naver.com>
2026-08-06 13:26:34 +0900

    update hi-melody font by next js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,17 @@
+import { Hi_Melody } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+
+/* 폰트 적용 */
+const hi_melody = Hi_Melody({
+  variable: "--font-hi-melody",
+  subsets: ["latin"], weight: "400"
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={hi_melody.variable}>
         <Header />
         {children}</body>
     </html>
