296fef6daf803abfa8d9573bfea455d0073f6f7d
Seop2 <lk9050@naver.com>
2026-08-14 11:03:01 +0900

    feat: add viewport meta tag to prevent user scaling on mobile devices

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,9 @@ const hi_melody = Hi_Melody({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </head>
       <body className={hi_melody.variable}>
         <Header />
         <RankingProvider>
