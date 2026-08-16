6c968603db272c6c4cc6dfcdd4c52def1aaacdde
Seop2 <lk9050@naver.com>
2026-08-16 18:19:02 +0900

    feat: add PWA manifest and icons for improved mobile app experience

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,11 +10,22 @@ const hi_melody = Hi_Melody({
   subsets: ["latin"], weight: "400"
 });
 
+export const metadata = {
+  manifest: "./manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default"
+  }
+}
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
       </head>
       <body className={hi_melody.variable}>
         <Header />
